290a83443c4961e8eaea11ae5281fcc749c803f4
Apo <eitel.trabajo@gmail.com>
2023-04-14 07:48:44 -0300

    se agregó diseño css al resto de la página, animaciones y el menu se cierra al navegar entre secciones

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
+const enlaces = document.querySelectorAll("#nav a");
 
 abrir.addEventListener('click', () => {
     nav.classList.add("visible");
 });
+
 cerrar.addEventListener('click', () => {
     nav.classList.remove("visible");
+});
+
+enlaces.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+        nav.classList.remove("visible");
+    });
 });
